7a34c168e75c78b814a012254ba744e310b97282
malprax <kingmalprax@gmail.com>
2015-08-19 08:49:39 +0800

    first modify

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,17 +9,17 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110804142407) do
+ActiveRecord::Schema.define(version: 20110804142407) do
 
-  create_table "clients", :force => true do |t|
+  create_table "clients", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "events", :force => true do |t|
+  create_table "events", force: :cascade do |t|
     t.datetime "start"
     t.datetime "end"
     t.string   "comment"
@@ -27,15 +28,15 @@ ActiveRecord::Schema.define(:version => 20110804142407) do
     t.datetime "updated_at"
   end
 
-  add_index "events", ["project_id"], :name => "index_events_on_project_id"
+  add_index "events", ["project_id"], name: "index_events_on_project_id"
 
-  create_table "projects", :force => true do |t|
+  create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.integer  "client_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "projects", ["client_id"], :name => "index_projects_on_client_id"
+  add_index "projects", ["client_id"], name: "index_projects_on_client_id"
 
 end
